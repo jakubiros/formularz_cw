6b6ab5fc445c89f7a36f2f35175fedcaf1911cc4
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2023-11-03 19:05:38 +0100

    Dodaj pliki projektów.

---
+++ formularz/Controllers/DaneController.cs
@@ -0,0 +1,34 @@
+﻿using formularz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace formularz.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+
+            return View(dane);
+        }
+
+    }
+}
